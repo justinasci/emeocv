c3dd79289cb7f4ecd5331fff5b6f1bbe94c05f1d
Justinas Cicėnas <cicenasjustinas@gmail.com>
2018-05-08 21:35:30 +0300

    REM: parsing date from filename

--- ImageInput.cpp
+++ ImageInput.cpp
@@ -57,18 +57,7 @@ bool DirectoryInput::nextImage() {
 
     _img = cv::imread(path.c_str());
 
-    // read time from file name
-    struct tm date;
-    memset(&date, 0, sizeof(date));
-    date.tm_year = atoi(_itFilename->substr(0, 4).c_str()) - 1900;
-    date.tm_mon = atoi(_itFilename->substr(4, 2).c_str()) - 1;
-    date.tm_mday = atoi(_itFilename->substr(6, 2).c_str());
-    date.tm_hour = atoi(_itFilename->substr(9, 2).c_str());
-    date.tm_min = atoi(_itFilename->substr(11, 2).c_str());
-    date.tm_sec = atoi(_itFilename->substr(13, 2).c_str());
-    _time = mktime(&date);
-
-    log4cpp::Category::getRoot() << log4cpp::Priority::INFO << "Processing " << *_itFilename << " of " << ctime(&_time);
+    log4cpp::Category::getRoot() << log4cpp::Priority::INFO << "Processing " << *_itFilename;
 
     // save copy of image if requested
     if (!_outDir.empty()) {
